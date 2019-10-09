1b81fddc8e34a1a3d82de9d7db6634470015f0e9
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-09 13:28:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -53,6 +53,7 @@ class Song
     parse = filename.split(" - ")
     artist = Artist.find_or_create_by_name(parse[0])
     genre = Genre.find_or_create_by_name(parse[2].split(".mp3").join)
+    self.new()
     
     # artist = parse[0]
     # song = parse[1]
