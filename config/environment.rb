7cb981e8fa47270532205600db17a765f0bee9bb
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-09 12:41:36 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -5,14 +5,15 @@ module Concerns
   module Findable 
     
     def find_by_name(name)
-      self.all.detect {|song| song.name == name} 
+      all.detect {|song| song.name == name} 
     end 
     
     def find_or_create_by_name(name)
       if find_by_name(name).nil?
-        self.create(name)
+        create(name)
       else 
-        
+        find_by_name(name)
+      end 
     end 
     
   end 
