8c46a0016ebd90bc03e2bcc5b1653f2dd7fc3796
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-09 14:07:47 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -32,7 +32,7 @@ class MusicLibraryController
   end 
   
   def list_genres 
-    Genre.all.sort {|a,b| a.name <=> b.name}.each_with_index {|genre, index} puts "#{index + 1}. #{genre.name}"}
+    Genre.all.sort {|a,b| a.name <=> b.name}.each_with_index {|genre, index| puts "#{index + 1}. #{genre.name}"}
   end 
   
   def list_songs_by_artist 
