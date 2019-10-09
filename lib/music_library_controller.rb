c5e8749665ddb4092c82710ac68b9f86ecd7d55f
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-09 13:40:54 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,7 +1,7 @@
 class MusicLibraryController 
   attr_reader :path 
   
-  def initialize(path='./db/mp3s/')
+  def initialize(path='./db/mp3s')
     @path = path 
     MusicImporter.new(path).import 
   end 
