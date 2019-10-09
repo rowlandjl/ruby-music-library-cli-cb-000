0f2c31ce5e22ee1a7bc59fc0b939f267fc09a060
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-09 14:27:24 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -55,7 +55,7 @@ class MusicLibraryController
     
     array = Song.all.sort{|a,b| a.name <=> b.name} 
     song = array[input-1]
-    puts "Playing #{song.name}"
+    puts "Playing #{song.name} by #{song.artist.name}"
   end 
   
 end 
