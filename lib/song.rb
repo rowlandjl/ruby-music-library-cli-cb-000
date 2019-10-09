e77adca922c1f598fddb4724173dd4d3d91e422e
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-09 12:37:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,6 +42,11 @@ class Song
   end 
   
   def self.find_or_create_by_name(name)
+    if self.find_by_name(name).nil? 
+      self.create(name)
+    else 
+      self.find_by_name(name)
+    end 
   end 
   
 end 
