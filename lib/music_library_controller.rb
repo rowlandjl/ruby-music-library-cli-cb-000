b4a2b4c0af8088101fa6fd7a4484d951dfb3673b
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-09 14:10:22 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -38,6 +38,8 @@ class MusicLibraryController
   def list_songs_by_artist 
     puts "Please enter the name of an artist:"
     input = gets.strip 
+    
+    
   end 
   
   def list_songs_by_genre
